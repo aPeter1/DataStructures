30034def757db91f69981e58a589dc98b9e071b1
Alec Petersen <k.alecpetersen@gmail.com>
2020-03-13 18:26:31 -0600

    Cleaned up BinaryHeap.c

--- Heap/BinaryHeap.c
+++ Heap/BinaryHeap.c
@@ -24,43 +24,6 @@ struct Heap {
 
 // Private Functions
 
-static void printNode(Node* node, int printChildren) {
-	if(node->parent != NULL) {
-		printf("%p %d, parent=%d, ", node, *((int*) node->data), *((int*) node->parent->data));
-	} else {
-		printf("%p %d, root, ", node, *((int*) node->data));
-	}
-	
-	if(node->leftChild != NULL) {
-		printf("children= L %d ", *((int*) node->leftChild->data));
-	} else {
-		printf("children= L None ");
-	}
-	
-	if(node->rightChild != NULL) {
-		printf(", R %d\n", *((int*) node->rightChild->data));
-	} else {
-		printf(", R None\n");
-	}
-	
-	
-	if(printChildren == 1) {
-		if(node->leftChild != NULL) {
-			printNode(node->leftChild, 1);
-		}
-		
-		if(node->rightChild != NULL) {
-			printNode(node->rightChild, 1);
-		}
-	}
-}
-
-static void printHeap(Heap* heap) {
-	if(heap->rootNode != NULL) {
-		printNode(heap->rootNode, 1);
-	}
-}
-
 static void switchData(Node* nodeOne, Node* nodeTwo) {
 	void* temporaryData = nodeOne->data;
 	nodeOne->data = nodeTwo->data;
@@ -267,36 +230,6 @@ void* heappeek(Heap* heap) {
 	return heap->rootNode->data;
 }
 
-void heapremove(Heap* heap, void* data) {
-	heap->size--;
-}
-
 int heaplen(Heap* heap) {
 	return heap->size;
-}
-
-// Testing
-
-int testCompare(void* val1, void* val2) {
-	return *((int*) val1) - *((int*) val2);
-}
-
-int main(int argc, char *argv[]) {
-	
-	int values[] = {3, 5, 1, 52, 2, 33, 51, 0, 522, 23};
-	
-	Heap* heap = heapify(testCompare, values, sizeof(values)/sizeof(values[0]), sizeof(int));			
-	
-	//printf("Peeking = %d\n", *((int*) heappeek(heap)));
-	printHeap(heap);
-	for(int i = 0; i < 1; i++) {
-		printf("\nPopping = %d\n", *((int*) heappop(heap)));
-		printHeap(heap);
-	}
-	printf("Size of heap is %d\n", heaplen(heap));
-	
-	heapfree(heap);
-	
-	printf("No errors!!\n");
-	return 0;
 }
